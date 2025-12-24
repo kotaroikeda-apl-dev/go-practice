1e7e02168e22307afae29da888084036c2bc6462
Kotaro Ikeda <ikeda.apl.development@gmail.com>
2025-12-25 07:27:03 +0900

    Type Switchのサンプルを追加

---
+++ interfaces/basic/type-switch/README.md
@@ -0,0 +1,20 @@
+## Type Switch のサンプル例
+
+- interface{} に入った複数の型を type switch で振り分ける
+- string/int/float64/[]byte/nil/未知型 をまとめて処理
+
+## 実行
+
+```bash
+go run main.go
+```
+
+## 出力例
+
+```
+nil value
+string len=6 val="gopher"
+int doubled=42
+float ceil-ish=4
+bytes len=3
+```
---
+++ interfaces/basic/type-switch/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// type switch を使って interface{} に入った複数の型をまとめて処理する例
+func main() {
+	values := []interface{}{
+		nil,
+		"gopher",
+		21,
+		3.14,
+		[]byte{1, 2, 3},
+	}
+
+	for _, v := range values {
+		describe(v)
+	}
+}
+
+func describe(v interface{}) {
+	switch vv := v.(type) {
+	case nil:
+		fmt.Println("nil value")
+	case string:
+		fmt.Printf("string len=%d val=%q\n", len(vv), vv)
+	case int:
+		fmt.Printf("int doubled=%d\n", vv*2)
+	case float64:
+		fmt.Printf("float ceil-ish=%d\n", int(vv)+1)
+	case []byte:
+		fmt.Printf("bytes len=%d\n", len(vv))
+	default:
+		fmt.Printf("unknown type %T\n", vv)
+	}
+}
+
